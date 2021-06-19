6f34b01ef56b00d4313cf7329b5a7b418b13e17b
quangdm <qduongminh3@gmail.com>
2021-06-19 18:50:49 +0700

    Hover effect. Loading effect

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let dropArea = document.getElementById('drop-area');
-let imgCount = 0;
+let uploadArea = document.querySelector(".upload-area");
+let divCount = 0;
+let divArray = [];
 function preventDefaults(e) {
     e.preventDefault();
     e.stopPropagation();
 }
-
 function highlight(e) {
     dropArea.classList.add('highlight');
 }
@@ -19,25 +20,60 @@ function handleDrop(e) {
     handleFiles(files);
 }
 function handleFiles(files) {
-    let myForm = dropArea.querySelector(".my-form");
-    myForm.classList.add("hide");
     dropArea.classList.add("grid-ctn");
-    files = [...files]; 
+    files = [...files];
+    files.forEach(uploadFile);
     files.forEach(previewFile);
+}
+function uploadFile(file) {
+    let url = 'YOUR URL HERE';
+    let formData = new FormData();
+
+    formData.append('file', file);
+
+    fetch(url, {
+        method: 'POST',
+        body: formData
+    })
+        .then(() => { /* Done. Inform the user */ })
+        .catch(() => { /* Error. Inform the user */ });
 }
 function previewFile(file) {
     let reader = new FileReader();
     reader.readAsDataURL(file);
-    reader.onloadend = function () {
-        let div = document.createElement('DIV');
-        imgCount++;
-        div.style.background = `url("${reader.result}") center`;
-        if(imgCount === 1) {
-            div.style.gridColumn = "1/3";
-            div.style.gridRow = "1/3";
+    let div = document.createElement('DIV');
+    let loader = document.createElement('DIV');
+    let loadStatus = document.createElement('DIV');
+    reader.onloadstart = function () {
+        loader.classList.add("loader");
+        loadStatus.innerHTML = "Uploading...";
+        if (divCount === 0) {
+            div.style.gridColumn = "1/span 2";
+            div.style.gridRow = "1/span 2";
             div.style.minHeight = "525px";
         }
-        dropArea.appendChild(div);
+        div.classList.add("imgDiv");
+        div.style.flexFlow = "column";
+        div.appendChild(loader);
+        div.appendChild(loadStatus);
+        dropArea.replaceChild(div, uploadArea);
+        uploadArea.classList.add("dashedBorder");
+        dropArea.classList.remove("dashedBorder");
+        dropArea.appendChild(uploadArea);
+    }
+    reader.onloadend = function () {
+        div.removeChild(loader);
+        div.removeChild(loadStatus);
+        // add div in divArray
+        divArray.splice(divCount,0, { bgImg: reader.result});
+        // style div
+        div.innerHTML = `<input type="checkbox">
+        <img src="zoom.png" alt="">`;
+        div.style.backgroundImage = `url("${divArray[divCount].bgImg}")`;
+        // style first div
+        div.draggable = "true";
+        // insert div in dropArea
+        divCount++;
     }
 }
 // preventDefaults
