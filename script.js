efc893124c4ed20b3ae8882445a2f2255a4a30fd
quangdm <qduongminh3@gmail.com>
2021-06-20 11:46:56 +0700

    Change image's order done

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let dropArea = document.getElementById('drop-area');
 let uploadArea = document.querySelector(".upload-area");
 let divCount = 0;
 let divArray = [];
+var draggedDiv;
 function preventDefaults(e) {
     e.preventDefault();
     e.stopPropagation();
@@ -47,38 +48,67 @@ function previewFile(file) {
     reader.onloadstart = function () {
         loader.classList.add("loader");
         loadStatus.innerHTML = "Uploading...";
-        if (divCount === 0) {
-            div.style.gridColumn = "1/span 2";
-            div.style.gridRow = "1/span 2";
-            div.style.minHeight = "525px";
-        }
         div.classList.add("imgDiv");
         div.style.flexFlow = "column";
         div.appendChild(loader);
         div.appendChild(loadStatus);
         dropArea.replaceChild(div, uploadArea);
+        dropArea.children[0].style.gridColumn = "1/span 2";
+        dropArea.children[0].style.gridRow = "1/span 2";
+        dropArea.children[0].style.minHeight = "525px";
+        // change dashed border
         uploadArea.classList.add("dashedBorder");
         dropArea.classList.remove("dashedBorder");
         dropArea.appendChild(uploadArea);
+        divArray.splice(divCount, 0, { id: divCount });
+        div.id = `${divCount}`;
+        divCount++;
     }
     reader.onloadend = function () {
         div.removeChild(loader);
         div.removeChild(loadStatus);
-        // add div in divArray
-        divArray.splice(divCount,0, { bgImg: reader.result});
+        div.addEventListener("dragstart", () => {
+            draggedDiv = div;
+        }, false);
+        div.addEventListener("dragenter", swapDiv,false);
         // style div
         div.innerHTML = `<input type="checkbox">
         <img src="zoom.png" alt="">`;
-        div.style.backgroundImage = `url("${divArray[divCount].bgImg}")`;
+        divArray[div.id].bgImg = reader.result;
+        div.style.backgroundImage = `url("${reader.result}")`;
         // style first div
         div.draggable = "true";
-        // insert div in dropArea
-        divCount++;
+    }
+}
+function swapDiv(e) {
+    let targetDiv = e.target;
+    if (targetDiv.id != draggedDiv.id) {
+        draggedPosition = parseInt(draggedDiv.id);
+        targetPosition = parseInt(targetDiv.id);
+        var draggedValue = divArray[draggedPosition];
+        if (draggedPosition < targetPosition) {
+            for (let i = draggedPosition; i < targetPosition; i++) {
+                divArray[i] = divArray[i + 1];
+            }
+            divArray[targetPosition] = draggedValue;
+        } else {
+            for (let i = draggedPosition; i > targetPosition; i--) {
+                divArray[i] = divArray[i - 1];
+            }
+            divArray[targetPosition] = draggedValue;
+        }
+        draggedDiv = targetDiv;
+        render();
+    }
+}
+function render() {
+    for (let i = 0; i < divArray.length; i++) {
+        dropArea.children[i].style.backgroundImage = `url("${divArray[i].bgImg}")`;
     }
 }
 // preventDefaults
 ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-    dropArea.addEventListener(eventName, preventDefaults, false);
+    document.addEventListener(eventName, preventDefaults, false);
 });
 // highlight
 ['dragenter', 'dragover'].forEach(eventName => {
@@ -89,3 +119,7 @@ function previewFile(file) {
 });
 // handleDrop
 dropArea.addEventListener('drop', handleDrop, false);
+// drag div
+document.addEventListener("drag", function (event) {
+
+}, false);
